4945fe3e0630c47c77660b67e885b41edda973e9
connergillette <connerwgillette@gmail.com>
2023-06-20 21:14:26 -0700

    Add OpenAI API for topic summaries (not called yet)

--- app/routes/$topic.tsx
+++ app/routes/$topic.tsx
@@ -3,6 +3,8 @@ import { useLoaderData } from '@remix-run/react'
 import Note, { NoteData } from '~/components/Note'
 import { supabase } from '~/server/supabase.server'
 
+import { fetchTopicSummary } from '~/server/openai.server'
+
 interface Props {
 
 }
@@ -11,29 +13,44 @@ export const loader: LoaderFunction = async ({ params }: LoaderArgs) => {
   const { topic } = params
 
   let notes : NoteData[] = []
+  let summary : string = ''
   if (topic) {
     const notesResponse = await supabase.from('notes').select().textSearch('text', topic)
     if (!notesResponse.error) {
       notes = notesResponse.data
+
+      // TODO: Implement stored summary system to only generate a summary when the topic notes change
+      // summary = await fetchTopicSummary(notes.map((note) => note.text))
+    } else {
+      return { topic, error: notesResponse.error }
     }
     // TODO: Implement error handling here
   }
-
+  
   return { topic, notes }
 }
 
 export default function Topic() {
-  const { topic, notes } = useLoaderData()
+  const { topic, notes, error } = useLoaderData()
   return (
     <div className="flex flex-col gap-6 px-2 my-5">
       <h1 className="text-2xl">{topic}</h1>
       <div className="flex gap-4">
-        <div className="w-1/2 bg-gray-100 rounded-md">
-          {notes.map((note: NoteData) => <Note data={note} key={note.id}/>)}
-        </div>
-        <div>
-          <h2 className="text-lg">Summary</h2>
-        </div>
+        {error && (
+          <div className="text-red-400 text-lg h-full w-full text-center justify-center my-20">
+            There was a problem fetching notes for this topic.
+          </div>
+        )}
+        {!error && (
+          <>
+            <div className="w-1/2 bg-gray-100 rounded-md">
+              {notes.map((note: NoteData) => <Note data={note} key={note.id}/>)}
+            </div>
+            <div>
+              <h2 className="text-lg">Summary</h2>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
---
+++ app/server/openai.server.tsx
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+export const fetchTopicSummary = async (topicNotes: string[]) => {
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ 
+        role: "user",
+        content: `\
+          Please combine my following notes (which are in reverse chronological order), without omitting any details:\
+          ${
+            topicNotes.join('\n')
+          }
+        `
+      }],
+    })
+  
+    return response.data.choices[0].content
+  } catch (e) {
+    console.log(e)
+  }
+}
